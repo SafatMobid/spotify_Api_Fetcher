e53005cd258f928e94c0ad53ea8e529bdd707bad
safat mobid <safatmobid@gmail.com>
2025-06-02 17:07:13 -0400

    Gets playlists, Need to fix error when non-standard playlist names

--- playlist_Scraper.py
+++ playlist_Scraper.py
@@ -5,16 +5,21 @@ from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = "yourClientID"
 CLIENT_SECRET = "yorClientSecret"
-REDIRECT_URL = "http://127.0.0.1/callback"
+REDIRECT_URL = "http://127.0.0.1:8000/callback"
 SCOPE = 'playlist-read-private' #-------> Used to get to playlist
 
 #------------AUTH TO LOG IN ------------------#
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URL,
-                                               scope=SCOPE))
+                                               scope=SCOPE))  #Access to the API
 
 # -----------------TEST-----------------#
 # Get current user info
 user = sp.current_user()
 print(f"Logged in as: {user['display_name']} ({user['id']})")
+
+# Get a list of your playlists
+playlists = sp.current_user_playlists()
+for playlist in playlists['items']:
+    print(f"- {playlist['name']} (ID: {playlist['id']})")
